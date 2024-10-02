92fe4a9419de06cb17a01fcc01e9969f0c70153f
Sparkling24 <111319163+Sparkling24@users.noreply.github.com>
2024-10-03 00:54:02 +0100

    Update 8-getBudgetCurrentYear.js

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -5,6 +5,7 @@ function getCurrentYear() {
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
+    
     'income-2021': 2100,
       'gdp-2021': 5200,
     'capita-2021': 1090 
