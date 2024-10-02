6d3474b50b3f8e1f23500700095028ee580b92e0
Sparkling24 <111319163+Sparkling24@users.noreply.github.com>
2024-10-02 23:42:26 +0100

    Update 3-default-parameter.js

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,10 +1,3 @@
-export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
-  if (expansion1989 === undefined) {
-    expansion1989 = 89;
-  }
-
-  if (expansion2019 === undefined) {
-    expansion2019 = 19;
-  }
+export default function getSumOfHoods(initialNumber, expansion1989 = 3, expansion2019 = 8) {
   return initialNumber + expansion1989 + expansion2019;
 }
