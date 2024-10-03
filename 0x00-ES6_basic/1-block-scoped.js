5fe699e6b4c2f148d1580306c00ad4d2ff92c332
Sparkling24 <111319163+Sparkling24@users.noreply.github.com>
2024-10-03 14:41:03 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,11 +2,11 @@ export default function taskBlock(trueOrFalse) {
   let task = false; // Change var to let
   let task2 = true; // Change var to let
 
+  return [task, task2];
+  
   if (trueOrFalse) {
     task = true; // Reassigning the existing variable
     task2 = false; // Reassigning the existing variable
- 
-    return [task, task2];
   }
 
   return [task, task2];
