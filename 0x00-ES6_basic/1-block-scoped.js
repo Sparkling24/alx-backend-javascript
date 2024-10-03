134141b7034b1fe44651640616bbc4b8dd260ac7
Sparkling24 <111319163+Sparkling24@users.noreply.github.com>
2024-10-03 13:57:52 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,5 +1,5 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false; // Change var to let
+  let task = true; // Change var to let
   let task2 = true; // Change var to let
 
   if (trueOrFalse) {
