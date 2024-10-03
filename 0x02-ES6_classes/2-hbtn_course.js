c56896a54d80e53c737df8b115126c0202f70007
Sparkling24 <111319163+Sparkling24@users.noreply.github.com>
2024-10-03 20:20:08 +0100

    Update 2-hbtn_course.js

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,71 +1,48 @@
-/**
- * Represents a Holberton Course.
- */
-export default class HolbertonCourse {
-  /**
-   * Creates a new @see {@link HolbertonCourse}.
-   *
-   * @param {String} name - The name of the course.
-   * @param {Number} length - How long the course is (in months).
-   * @param {String[]} students - The names of students in the course.
-   */
-  constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
-  }
+class HolbertonCourse:
+    def __init__(self, name, length, students):
+        self._name = None
+        self._length = None
+        self._students = None
 
-  /**
-   * Gets the name of this course.
-   */
-  get name() {
-    return this._name;
-  }
+        self.name = name  # Uses the setter to validate and set the value
+        self.length = length  # Uses the setter to validate and set the value
+        self.students = students  # Uses the setter to validate and set the value
 
-  /**
-   * Sets the name of this course.
-   */
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = value;
-  }
+    @property
+    def name(self):
+        return self._name
 
-  /**
-   * Gets the length of this course (in months).
-   */
-  get length() {
-    return this._length;
-  }
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str):
+            raise ValueError("name must be a string")
+        self._name = value
 
-  /**
-   * Sets the length of this course (in months).
-   */
-  set length(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = value;
-  }
+    @property
+    def length(self):
+        return self._length
 
-  /**
-   * Gets the names of students in this course.
-   */
-  get students() {
-    return this._students;
-  }
+    @length.setter
+    def length(self, value):
+        if not isinstance(value, (int, float)):
+            raise ValueError("length must be a number")
+        self._length = value
 
-  /**
-   * Sets the names of students in this course.
-   */
-  set students(value) {
-    if (!(value instanceof Array)) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    if (!value.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    this._students = value;
-  }
-    }
+    @property
+    def students(self):
+        return self._students
+
+    @students.setter
+    def students(self, value):
+        if not isinstance(value, list) or not all(isinstance(student, str) for student in value):
+            raise ValueError("students must be a list of strings")
+        self._students = value
+
+# Example usage
+try:
+    course = HolbertonCourse("Python Programming", 10, ["Alice", "Bob"])
+    print(course.name)     # Output: Python Programming
+    print(course.length)   # Output: 10
+    print(course.students)  # Output: ['Alice', 'Bob']
+except ValueError as e:
+    print(e)
