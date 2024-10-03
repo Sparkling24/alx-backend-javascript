ace739e71cccc536205c4bdf34dc5a8623c8f7e0
Sparkling24 <111319163+Sparkling24@users.noreply.github.com>
2024-10-03 14:53:35 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -18,8 +18,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true; // eslint-disable-line no-unused-vars
-    let task2 = false; // eslint-disable-line no-unused-vars
+    let task = false; // eslint-disable-line no-unused-vars
+    let task2 = true; // eslint-disable-line no-unused-vars
   }
 
   return [task, task2];
