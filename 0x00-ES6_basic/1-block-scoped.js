1b82e47b1fcdbd6e6cde12579014bdb1c0298b0c
Sparkling24 <111319163+Sparkling24@users.noreply.github.com>
2024-10-04 04:06:33 +0100

    Update 1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,12 @@
-export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
+export default function getSanFranciscoDescription() {
+  const year = 2017;
+  const budget = {
+    income: '$119,868',
+    gdp: '$154.2 billion',
+    capita: '$178,479',
+  };
 
-  if (trueOrFalse) {
-    const task = true; // eslint-disable-line no-unused-vars
-    const task2 = false; // eslint-disable-line no-unused-vars
-  }
-
-  return [task, task2];
+  return `As of ${year}, it was the seventh-highest income county in the United States\
+, with a per capita personal income of ${budget.income}. As of 2015, San Francisco \
+proper had a GDP of ${budget.gdp}, and a GDP per capita of ${budget.capita}.`;
 }
